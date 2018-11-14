f6405299898d7157ddda5d9e80bd413bae668b6b
Andres <andres.piraquive@emyode.com>
2018-11-14 17:37:44 -0500

    Add unit tests

--- src/main/java/org/tp3_mgl7460/domain/Geologue.java
+++ src/main/java/org/tp3_mgl7460/domain/Geologue.java
@@ -9,32 +9,53 @@ package org.tp3_mgl7460.domain;
  *
  * @author sergedelil
  */
-public class Geologue extends Membre{
-    
-    private final int heureMinCycle = 55;
-    
-    public Geologue(String nom, String prenom, int sexe, String cycle, String numeroPermis) {
+public class Geologue extends Membre {
+
+    public final int heureMinCycle = 55;
+    int heureCycle;
+
+    public Geologue(String nom, String prenom, int sexe, String cycle, String numeroPermis, int heureCycle) {
         super(nom, prenom, sexe, cycle, numeroPermis);
+        this.heureCycle = heureCycle;
     }
-    
+
     @Override
-    public boolean validerCycle() {
-        return false;
+    public boolean validerCycle() {        
+        boolean isValiderCycle = false;
+        
+        if(cycle == "2013-2016")
+        {
+            isValiderCycle = true;
+        }
+        return isValiderCycle;
     }
 
     @Override
     public boolean validerNumeroPermis() {
+        String firstLetterNom = nom.substring(0, 1);
+        String firstLetterPrenom = prenom.substring(0, 1);
+
+        if (Character.isUpperCase(firstLetterNom.charAt(0)) && Character.isUpperCase(firstLetterPrenom.charAt(0))) {
+            String firstCharacterNomAndPrenom = firstLetterNom + firstLetterPrenom;
+
+            return (String.valueOf(numeroPermis).matches(firstCharacterNomAndPrenom + "[0-9]{4}"));
+        }
         return false;
     }
 
     @Override
     public boolean atteintHeureMinCycle() {
-        return false;
+        boolean isAtteintHeureMinCycle = false;
+
+        if (heureCycle >= heureMinCycle) {
+            isAtteintHeureMinCycle = true;
+        };
+        return isAtteintHeureMinCycle;
     }
 
     @Override
     public String toString() {
         return "Geologue{" + "nom=" + nom + ", prenom=" + prenom + ", sexe=" + sexe + ", cycle=" + cycle + ", numeroPermis=" + numeroPermis + ", heureMinCycle=" + heureMinCycle + ", activites=" + activites + '}';
     }
-    
+
 }
---
+++ src/test/java/org/tp3_mgl7460/domain/GeologueTest.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.tp3_mgl7460.domain;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author andres.piraquive
+ */
+public class GeologueTest {
+    Geologue geologue;
+    
+  
+    public GeologueTest() {        
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {       
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+         geologue = new Geologue("Piraquive","Andres",1,"2013-2016","PA3223", 55);     
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of validerCycle method, of class Geologue.
+     */
+    @Test
+    public void testValiderCycle() {
+       assertEquals(true, geologue.validerCycle());  
+    }
+
+    /**
+     * Test of validerNumeroPermis method, of class Geologue.
+     */
+    @Test
+    public void testValiderNumeroPermis() {     
+        assertEquals(true, geologue.validerNumeroPermis());       
+    }
+
+    /**
+     * Test of atteintHeureMinCycle method, of class Geologue.
+     */
+    @Test
+    public void testAtteintHeureMinCycle() {
+         assertEquals(true, geologue.atteintHeureMinCycle()); 
+    }
+
+    /**
+     * Test of toString method, of class Geologue.
+     */
+    @Test
+    public void testToString() {
+        assertEquals("Geologue{" + "nom=" + geologue.nom 
+                                 + ", prenom=" + geologue.prenom 
+                                 + ", sexe=" + geologue.sexe 
+                                 + ", cycle=" + geologue.cycle 
+                                 + ", numeroPermis=" + geologue.numeroPermis 
+                                 + ", heureMinCycle=" + geologue.heureMinCycle 
+                                 + ", activites=" + geologue.activites 
+                                 + '}', geologue.toString()); 
+    }
+    
+}
